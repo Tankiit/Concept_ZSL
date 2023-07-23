7dcf468048778e8c818216cf10a6e4d74286138b
RyuGood0 <ryugood0@gmail.com>
2023-07-23 17:19:13 +0200

    Julia data cleaning

--- src/clean_data.jl
+++ src/clean_data.jl
@@ -25,25 +25,32 @@ for i in 1:size(predicate_matrix, 1)
 end
 
 function get_direct_subsets(feature_set::Set{Int8})
-    subsets = Array{Set{Int8}}()
+    subsets = []
     for feature in feature_set
-        push!(subsets, Set{Int8}([feature]))
+        new_set = deepcopy(feature_set)
+        delete!(new_set, feature)
+        push!(subsets, new_set)
     end
     return subsets
 end
 
-function extend_feature_bank(feature_bank)
-    for (class_, feature_list) in feature_bank
+using ProgressBars
+
+function extend_feature_bank(feature_bank::Dict, max_new_features=100)
+    for i in ProgressBar(1:length(classes))
+        class_ = classes[i]
+        feature_list = feature_bank[class_]
+
         new_feature_list = [feature_list[1]]
         source_feature_sets = [feature_list2[1] for (class2, feature_list2) in feature_bank if class_ != class2]
 
         subsets = get_direct_subsets(feature_list[1])
         while length(subsets) > 0
-            new_subsets = Array{Set{Int8}}()
+            new_subsets = []
             for subset in subsets
                 to_add = true
                 for source_feature_set in source_feature_sets
-                    if subset.issubset(source_feature_set)
+                    if issubset(subset, source_feature_set)
                         to_add = false
                         break
                     end
@@ -54,6 +61,10 @@ function extend_feature_bank(feature_bank)
                 end
             end
             subsets = new_subsets
+
+            if length(new_feature_list) >= max_new_features
+                break
+            end
         end
 
         feature_bank[class_] = new_feature_list
@@ -62,4 +73,18 @@ function extend_feature_bank(feature_bank)
     return feature_bank
 end
 
+println("Extending feature bank...")
 feature_bank = extend_feature_bank(feature_bank)
+
+# Save feature bank to json
+println("Saving data...")
+using JSON
+
+data = Dict{String, Any}()
+data["features"] = features
+data["classes"] = classes
+data["feature_bank"] = feature_bank
+
+open("datasets/Animals_with_Attributes2/data.json", "w") do f
+    JSON.print(f, data)
+end
